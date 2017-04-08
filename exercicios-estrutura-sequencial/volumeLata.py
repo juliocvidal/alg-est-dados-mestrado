2a314b11468e609eb96b3cf3d896e95e042e9329
juliocvidal <juliocvidal@gmail.com>
2017-04-08 19:46:06 -0300

    volume da lata

---
+++ exercicios-estrutura-sequencial/volumeLata.py
@@ -0,0 +1,7 @@
+raio = float(input("Informe o raio da lata: "))
+altura = float(input("Informe a altura da lata: "))
+
+pi = 3.14159
+volume = pi * raio*raio * altura
+
+print("Volume da lata: ", volume)
