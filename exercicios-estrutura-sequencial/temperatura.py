85801a5fd190cd8dfa11d7d9fcf0eff7cbea790c
juliocvidal <juliocvidal@gmail.com>
2017-04-08 19:39:34 -0300

    solucao para o problema da temperatura em centigrados e farenheint

---
+++ exercicios-estrutura-sequencial/temperatura.py
@@ -0,0 +1,5 @@
+temperaturaEmGrausCentigrados = float(input("Informe a temperatura em graus centigrados: "))
+
+temperaturaEmFarenheint = (9*temperaturaEmGrausCentigrados+160)/5
+
+print("Temperatura em Farenheint: ", temperaturaEmFarenheint)
