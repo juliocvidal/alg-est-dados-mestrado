a2e32723b83c94bc40fde0ac75d771912a66d24c
juliocvidal <juliocvidal@gmail.com>
2017-04-08 19:26:45 -0300

    solucao para o problema dos carros e das cores

---
+++ exercicios-estrutura-sequencial/.idea/carros.py
@@ -0,0 +1,23 @@
+#artur #bernardo #cesar
+
+#palio #gol #fusion
+
+#cinza #verde #azul
+
+#artur #cinza
+#cesar #fusion
+#bernardo - nao verde e nao palio
+
+#cores do palio, gol e fusion?
+
+print("O carro de bernardo entao so pode ser o Gol azul.")
+print("Logo, o carro cinza de artur eh o Palio.")
+print("E o Fusion de cesar eh verde\n")
+
+print("Gol - Azul")
+print("Palio - Cinza")
+print("Fusion - Verde")
+
+
+
+
